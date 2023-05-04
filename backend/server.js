e3c2485675feaa9518786644e3aa8f7da2ef6f18
Hanni <ngynngchn@gmail.com>
2023-05-04 16:47:35 +0200

    added login route

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import "./utils/config.js";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import {
 	addProject,
 	getProjects,
@@ -9,14 +10,18 @@ import {
 	addSession,
 	endSession,
 	getSession,
-} from "./controller.js";
+} from "./controller/controller.js";
+import { encryptPassword } from "./middleware/authMiddleware.js";
+import { login } from "./controller/authController.js";
 
 const PORT = process.env.PORT;
 const server = express();
 
+//* ====== BODYPARSER =====
 server.use(morgan("dev"));
 server.use(express.json());
-server.use(cors({ origin: process.env.VITE_FRONTEND }));
+server.use(cookieParser());
+server.use(cors({ origin: process.env.VITE_FRONTEND, credentials: "include" }));
 
 //* ====== ROUTES ======
 server.post("/api/v1/add", addProject);
@@ -29,6 +34,8 @@ server.delete("/api/v1/delete", removeProject);
 server.post("/api/v1/start-session", addSession);
 server.post("/api/v1/end-session", endSession);
 
-//* ====== SERVER ======
+//* ====== AUTH ROUTES ======
+server.post("/api/v1/login", encryptPassword, login);
 
+//* ====== SERVER ======
 server.listen(PORT, () => console.log("Listening to Port:", PORT));
